96c010567671a540d613c02855de9e76c77602ee
gnoul_ <ttluong4869@gmail.com>
2021-10-18 11:06:21 +0700

    Setup loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project';
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [tours, SetTours] = useState([]);
+
+    if (loading) {
+        return (
+            <main>
+                <Loading />
+            </main>
+        );
+    }
+
     return <h2>Tours Project Setup</h2>;
 }
 
--- src/Tours.js
+++ src/Tours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
 const Tours = () => {
-  return <h2>tours component</h2>;
+    return <h2>tours component</h2>;
 };
 
 export default Tours;
